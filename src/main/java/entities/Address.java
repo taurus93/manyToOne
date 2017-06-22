65e67854dafbc9edbc1cfdd4f96b7be8ac3d4dc0
taurus93 <lethanhtunglc@gmail.com>
2017-06-22 09:17:47 +0700

    Initial commit

---
+++ src/main/java/entities/Address.java
@@ -0,0 +1,72 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Address {
+  private Long id;
+  private String street_name;
+  private String city_name;
+  private String state_name;
+  private String zipcode;
+
+  public Address() {}
+  public Address(String street, String city,
+                 String state, String zipcode) {
+    this.street_name = street;
+    this.city_name = city;
+    this.state_name = state;
+    this.zipcode = zipcode;
+  }
+
+  @Id
+  @Column
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  @Basic
+  @Column
+  public String getStreet_name() {
+    return street_name;
+  }
+
+  public void setStreet_name(String street_name) {
+    this.street_name = street_name;
+  }
+
+  @Basic
+  @Column
+  public String getCity_name() {
+    return city_name;
+  }
+
+  public void setCity_name(String city_name) {
+    this.city_name = city_name;
+  }
+
+  @Basic
+  @Column
+  public String getState_name() {
+    return state_name;
+  }
+
+  public void setState_name(String state_name) {
+    this.state_name = state_name;
+  }
+
+  @Basic
+  @Column
+  public String getZipcode() {
+    return zipcode;
+  }
+
+  public void setZipcode(String zipcode) {
+    this.zipcode = zipcode;
+  }
+}
---
+++ src/main/java/entities/Employee.java
@@ -0,0 +1,72 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Employee {
+  private Long id;
+  private String first_name;
+  private String last_name;
+  private int salary;
+  private Address address;
+
+  public Employee() {}
+  public Employee(String fname, String lname,
+                  int salary, Address address ) {
+    this.first_name = fname;
+    this.last_name = lname;
+    this.salary = salary;
+    this.address = address;
+  }
+
+  @Id
+  @Column
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  @Basic
+  @Column
+  public String getFirst_name() {
+    return first_name;
+  }
+
+  public void setFirst_name(String first_name) {
+    this.first_name = first_name;
+  }
+
+  @Basic
+  @Column
+  public String getLast_name() {
+    return last_name;
+  }
+
+  public void setLast_name(String last_name) {
+    this.last_name = last_name;
+  }
+
+  @Basic
+  @Column
+  public int getSalary() {
+    return salary;
+  }
+
+  public void setSalary(int salary) {
+    this.salary = salary;
+  }
+
+  @ManyToOne
+  @Column
+  public Address getAddress() {
+    return address;
+  }
+
+  public void  setAddress( Address address ) {
+    this.address = address;
+  }
+}
